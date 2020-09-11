492e20a7a3769a3bae93df42f64af9fc34db2769
nabiliwas <nabiliwas360@gmail.com>
2020-09-11 20:40:49 +0100

    little changes

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = 'polls'
+
+# router = DefaultRouter()
+# router.register('', views.PollViewSet)
 
 urlpatterns = [
     path('', views.PollsList.as_view(), name='polls_list'),
@@ -9,3 +13,6 @@ urlpatterns = [
     path('<int:pk>/choices/', views.ChoicesList.as_view(), name='choices_list'),
     path('<int:pk>/choices/<int:choice_pk>/vote/', views.CreateVote.as_view(), name='create_vote'),
 ]
+
+# urlpatterns += router.urls
+
--- side-notes/6-using routers and viewsets.py
+++ side-notes/6-using routers and viewsets.py
@@ -1,3 +1,29 @@
+
+# using router and viewsets simplify the code 
+
+###################### urls.py ########################
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+app_name = 'polls'
+
+router = DefaultRouter() # <----
+router.register('', views.PollViewSet) # <----
+
+urlpatterns = [
+    # path('', views.PollsList.as_view(), name='polls_list'), # <----
+    # path('<int:pk>/', views.PollDetail.as_view(), name='poll_detail'), # <-----
+    path('<int:pk>/choices/', views.ChoicesList.as_view(), name='choices_list'),
+    path('<int:pk>/choices/<int:choice_pk>/vote/', views.CreateVote.as_view(), name='create_vote'),
+]
+
+urlpatterns += router.urls # <----
+
+
+##################### views.py ##########################
+
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
@@ -7,6 +33,7 @@ from rest_framework.response import Response
 from .models import Poll, Choice, Vote
 from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 
+'''
 # retreive poll list or create poll instance
 class PollsList(generics.ListCreateAPIView):
     queryset = Poll.objects.all()
@@ -17,12 +44,13 @@ class PollsList(generics.ListCreateAPIView):
 class PollDetail(generics.RetrieveDestroyAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
+'''
 
 
-# using viewsets to simplify code 
-# class PollViewSet(viewsets.ModelViewSet):
-    # queryset = Poll.objects.all()
-    # serializer_class = PollSerializer
+class PollViewSet(viewsets.ModelViewSet): # <--------
+    queryset = Poll.objects.all() # <--------
+    serializer_class = PollSerializer # <--------
+
 
 # retreive choices list or create choice instance
 class ChoicesList(generics.ListCreateAPIView):
